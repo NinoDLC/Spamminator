efd738e62ac9a34d222284756925a092820b21dc
Nino <anthony@delcey.fr>
2023-01-06 16:58:20 +0100

    Improve resilience to exceptions

--- src/jvmMain/kotlin/data/liveinfo/LiveInfoRepository.kt
+++ src/jvmMain/kotlin/data/liveinfo/LiveInfoRepository.kt
@@ -2,7 +2,7 @@ package data.liveinfo
 
 import io.ktor.client.*
 import io.ktor.client.request.*
-import io.ktor.http.*
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
@@ -18,23 +18,19 @@ class LiveInfoRepository(
     val totalFailsFlow: Flow<Int> = totalFailsMutableStateFlow
 
     suspend fun getLiveInfo(stationId: String, rule: String, date: Long?, preset: String) {
-        println("getLiveInfo() called with: stationId = $stationId, rule = $rule, date = $date, preset = $preset")
-
-        val httpResponse = httpClient.get("https://api.radiofrance.fr/livemeta/live/$stationId/$rule") {
-            headers {
-                append(HttpHeaders.Accept, "application/json")
-                append(HttpHeaders.UserAgent, "Spamminator - ${javaClass.`package`.implementationVersion}")
+        try {
+            httpClient.get("https://api.radiofrance.fr/livemeta/live/$stationId/$rule") {
+                url {
+                    parameters.append("preset", preset)
+                    date?.let { parameters.append("date", date.toString()) }
+                }
             }
-            url {
-                parameters.append("preset", preset)
-                date?.let { parameters.append("date", date.toString()) }
-            }
-        }
-        totalCallsMutableStateFlow.update { it + 1 }
-        println("getLiveInfo() SUCCESS with: stationId = $stationId, rule = $rule, date = $date, preset = $preset")
-
-        if (!httpResponse.status.isSuccess()) {
+        } catch (cancellationException: CancellationException) {
+            throw cancellationException
+        } catch (e: Exception) {
             totalFailsMutableStateFlow.update { it + 1 }
+        } finally {
+            totalCallsMutableStateFlow.update { it + 1 }
         }
     }
 }
