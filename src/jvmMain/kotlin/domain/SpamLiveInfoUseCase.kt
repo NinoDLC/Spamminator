d345b8cbdd4d1d2502f18bc0ad97b6e7da985725
Nino <anthony@delcey.fr>
2023-01-06 16:59:36 +0100

    Improve performances for spam count flow, supports 'in-between' cancellation

--- src/jvmMain/kotlin/domain/SpamLiveInfoUseCase.kt
+++ src/jvmMain/kotlin/domain/SpamLiveInfoUseCase.kt
@@ -3,10 +3,9 @@ package domain
 import data.liveinfo.LiveInfoRepository
 import data.preset.PresetRepository
 import data.spam.SpamRepository
+import data.station.model.StationEntity
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.launch
-import java.util.concurrent.atomic.AtomicInteger
 import kotlin.time.Duration.Companion.seconds
 
 class SpamLiveInfoUseCase(
@@ -15,33 +14,33 @@ class SpamLiveInfoUseCase(
     private val presetRepository: PresetRepository,
 ) {
 
-    fun getSpamCountFlow(stationId: String): Flow<Int?> = channelFlow {
-        spamRepository.stationSpammedMapStateFlow
-            .map { stations -> stations.find { it.id == stationId } }
-            .distinctUntilChanged()
-            .collectLatest { stationEntity ->
-                if (stationEntity != null) {
-                    trySend(0)
-                    val count = AtomicInteger(0)
-
-                    while (true) {
-                        presetRepository.presetsFlow.value.forEach {
-                            launch {
-                                liveInfoRepository.getLiveInfo(
-                                    stationId = stationEntity.id,
-                                    rule = stationEntity.liveRule,
-                                    date = null,
-                                    preset = it.value
-                                )
-                                trySend(count.incrementAndGet())
-                            }
-                        }
-
-                        delay(10.seconds)
-                    }
-                } else {
-                    trySend(null)
-                }
+    fun getSpamCountFlow(stationId: String): Flow<Int?> = spamRepository.stationSpammedMapStateFlow
+        .map { stations -> stations.find { it.id == stationId } }
+        .distinctUntilChanged()
+        .flatMapLatest { stationEntity ->
+            if (stationEntity != null) {
+                getFlowsForPresets(stationEntity)
+                    .merge()
+                    .runningFold(0) { acc: Int, _: Unit -> acc + 1 }
+            } else {
+                flowOf(null)
             }
-    }.conflate()
+        }
+
+    private fun getFlowsForPresets(stationEntity: StationEntity) = presetRepository.presetsFlow.value.map { preset ->
+        flow {
+            while (true) {
+                liveInfoRepository.getLiveInfo(
+                    stationId = stationEntity.id,
+                    rule = stationEntity.liveRule,
+                    date = null,
+                    preset = preset.value
+                )
+
+                emit(Unit)
+
+                delay(10.seconds)
+            }
+        }
+    }
 }
